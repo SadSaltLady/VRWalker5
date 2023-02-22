064aa7758826b9e90d4de8ddcc69cbd6b0459fa4
SadSaltLady <62451205+SadSaltLady@users.noreply.github.com>
2023-02-22 00:41:22 -0500

    python code writes to csv

--- Python/Server.py
+++ Python/Server.py
@@ -27,24 +27,39 @@ def TransmitData(content):
     print(content)
     conn.send(bytes(str(content), 'utf-8'))  # echo
 
+def ProcessData(data): 
+    refined = data.split("?")
+    file = open(refined[0], 'w')
+    if len(refined) > 1: 
+        file.write(refined[1])
+        file.close() 
+    return refined[0]
+
+    
+
 print("starting")
 TCP_IP = '127.0.0.1'
 TCP_PORT = 54000
-BUFFER_SIZE = 50  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-print("Listening for clients, a confirmation message will appear below when someone connects to me! \n")
+print("Listening for clients, further messages will appear below when someone connects to me! \n")
 conn, addr = s.accept()
 print('Connection address:', addr)
-while 1:
+while True:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print("received data:", data)
-    query = cleanQuery(data)
-    if (validateQuery(query, 16)):
-        TransmitData("Data Received")
-    TransmitData(-1)
-    TransmitData(len(query))
+    str = data.decode()
+    print("received data:", str, "\n")
+    fileName = ProcessData(str)
+    print("processed data and created new file: " + fileName + "\n")
+    
+    
+    #query = cleanQuery(data)
+    #if (validateQuery(query, 16)):
+    #    TransmitData("Data Received")
+    #TransmitData(-1)
+    #TransmitData(len(query))
 conn.close()

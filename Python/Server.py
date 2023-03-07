7d1bfe8e2eb1d567c189e93da7d6972189138686
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2023-03-07 17:15:58 -0500

    Update Server.py

--- Python/Server.py
+++ Python/Server.py
@@ -83,7 +83,7 @@ def server():
     # AwaitingData = False
     # fileName = ""
 
-    TCP_IP = '127.0.0.1' #"192.168.137.37" #'127.0.0.1' local host value
+    TCP_IP = "192.168.137.37" #'127.0.0.1' local host value
     TCP_PORT = 54000
     BUFFER_SIZE = 16384  # Normally 1024, but we want fast response
 

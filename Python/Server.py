f8d8ed5f9ad027ebcad8688722a5ee3258f46efa
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2023-03-07 16:14:45 -0500

    json buffer implemented

--- Python/Server.py
+++ Python/Server.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import socket
+import queue
+import json
 
+
+DataQueue = queue.Queue()
+Delimiter = "!"
 FILE = None
+LeftOverData = ""
 
 def cleanQuery(query):
     temp = ""
@@ -24,14 +30,15 @@ def validateQuery(query, idealLength = -1):#tests
             return False
     return True
 
-def TransmitData(content):
-    print("Sending: \n")
-    print(content)
-    conn.send(bytes(str(content), 'utf-8'))  # echo
-
+# def TransmitData(content):
+#     print("Sending: \n")
+#     print(content)
+#     conn.send(bytes(str(content), 'utf-8'))  # echo
+#
 
 def ProcessData(data):
     data = data + "\n"
+    global FILE
     FILE.write(data)
     return data
 
@@ -41,45 +48,58 @@ def CreateFile(data):
     print(type(file))
     return filename, file
 
-    
-
-AwaitingFileName = False
-AwaitingData = False
-fileName = ""
-TCP_IP = "192.168.137.37" #'127.0.0.1' local host value
-TCP_PORT = 54000
-BUFFER_SIZE = 16384  # Normally 1024, but we want fast response
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-print("Listening for clients, further messages will appear below when someone connects to me! \n")
-conn, addr = s.accept()
-print('Connection address:', addr)
-while True:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
+def ProcessCommand(data):
+    # no guarantee that str contains the data we need, so we should make sure we get all we need.
     str = data.decode()
-    if (str == "start"):
-        AwaitingFileName = True
-    elif (str == "stop"):
-        AwaitingData = False
-        FILE.close()
-    elif AwaitingFileName:
-        fileName, f = CreateFile(str)
-        FILE = f
-        AwaitingFileName = False
-        AwaitingData = True
-    elif AwaitingData:
-        output = ProcessData(str)
-        print("Wrote: " + output)
-    
-    
- 
+    commands = str.split(Delimiter)
+    for i in range(len(commands)):
+        DataQueue.put(commands[i])
 
-    #query = cleanQuery(data)
-    #if (validateQuery(query, 16)):
-    #    TransmitData("Data Received")
-    #TransmitData(-1)
-    #TransmitData(len(query))
-conn.close()
+
+def ProcessQueue():
+    global FILE #uses the global FILE variable
+    #no valid commands inside the queue
+    if DataQueue.empty():
+        return
+    while not DataQueue.empty():
+        values = DataQueue.get()
+        # if command is start
+        if values.has_key("FileName"):
+            fileName, f = CreateFile(values["FileName"])
+            FILE = f
+            return
+        # if command is stop
+        if values.has_key("stop"):
+            FILE.close()
+            return
+        # if command is data
+        if values.has_key("data"):
+            ProcessData(values["data"])
+            return
+
+
+def server():
+    # AwaitingFileName = False
+    # AwaitingData = False
+    # fileName = ""
+
+    TCP_IP = '127.0.0.1' #"192.168.137.37" #'127.0.0.1' local host value
+    TCP_PORT = 54000
+    BUFFER_SIZE = 16384  # Normally 1024, but we want fast response
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+    print("Listening for clients, further messages will appear below when someone connects to me! \n")
+    conn, addr = s.accept()
+    print('Connection address:', addr)
+    # TODO: timeout and dump to file (if there is known filename) after no data after say... 5 seconds
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        #no guarantee data has complete commands, so needs to be processed
+        ProcessCommand(data)
+        ProcessQueue()
+    conn.close()
+
+server()
